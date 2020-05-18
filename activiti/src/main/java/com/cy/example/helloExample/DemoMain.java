4914ae570e46a7a5cde1c1dea298e033af78f51b
cy <theemailofcy@163.com>
2020-05-18 17:30:26 +0800

    更新

---
+++ activiti/src/main/java/com/cy/example/helloExample/DemoMain.java
@@ -0,0 +1,113 @@
+package com.cy.example.helloExample;
+
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.*;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
+import org.activiti.engine.impl.form.DateFormType;
+import org.activiti.engine.impl.form.StringFormType;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @program: activiti
+ * @description:
+ * @author: cy
+ * @create: 2020/05/18
+ **/
+@Slf4j
+public class DemoMain {
+
+    public static void main(String[] args) {
+        log.info("启动程序");
+        // 1. 创建流程引擎
+        ProcessEngine processEngine = getProcessEngine();
+
+        // 2. 部署流程定义文件, 获取流程定义对象
+        ProcessDefinition processDefinition = getProcessDefinition(processEngine);
+
+        // 3. 启动运行流程, 获取流程实例
+        ProcessInstance processInstance = getProcessInstance(processEngine, processDefinition);
+
+        // 4. 处理流程任务
+        Scanner scanner = new Scanner(System.in);
+        while (null != processInstance && !processInstance.isEnded()) {
+            TaskService taskService = processEngine.getTaskService();
+            List<Task> list = taskService.createTaskQuery().list();
+            log.info("待处理任务数量 [{}]", list.size());
+            for (Task task : list) {
+                log.info("待处理任务: [{}]", task.getName());
+                FormService formService = processEngine.getFormService();
+                TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+                List<FormProperty> formProperties = taskFormData.getFormProperties();
+                Map<String, Object> variables = Maps.newHashMap();
+                for (FormProperty formProperty : formProperties) {
+                    if (StringFormType.class.isInstance(formProperty.getType())) {
+                        log.info("请输入: [{}]", formProperty.getName());
+                        String line = scanner.nextLine();
+                        log.info("您输入的是: [{}]", line);
+                        variables.put(formProperty.getId(), line);
+                    } else if (DateFormType.class.isInstance(formProperty.getType())) {
+                        log.info("请输入: [{}], 格式: [yyyy-MM-dd]", formProperty.getName());
+                        String line = scanner.nextLine();
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                        Date parse = null;
+                        try {
+                            parse = format.parse(line);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        log.info("您输入的是: [{}]", line);
+                        variables.put(formProperty.getId(), parse);
+                    } else {
+                        log.info("类型暂不支持!", formProperty.getType());
+                    }
+                }
+                // 提交任务
+                taskService.complete(task.getId(), variables);
+                processInstance = processEngine.getRuntimeService().createProcessInstanceQuery().processInstanceId(processInstance.getId()).singleResult();
+            }
+        }
+        log.info("结束程序");
+    }
+
+    private static ProcessInstance getProcessInstance(ProcessEngine processEngine, ProcessDefinition processDefinition) {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+        log.info("启动流程 ==> [{}]", processInstance.getProcessDefinitionKey());
+        return processInstance;
+    }
+
+    private static ProcessDefinition getProcessDefinition(ProcessEngine processEngine) {
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+        deploymentBuilder.addClasspathResource("second_approve.bpmn20.xml");
+        Deployment deploy = deploymentBuilder.deploy();
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deploy.getId())
+                .singleResult();
+        log.info("流程定义对象 processDefinition ==> 流程定义文件: [{}], 流程 ID: [{}]", processDefinition.getName(), processDefinition.getId());
+        return processDefinition;
+    }
+
+    private static ProcessEngine getProcessEngine() {
+        ProcessEngineConfiguration configuration = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
+        ProcessEngine processEngine = configuration.buildProcessEngine();
+        String name = processEngine.getName();
+        String version = processEngine.VERSION;
+        log.info("process ==> name: [{}], version: [{}]", name, version);
+        return processEngine;
+    }
+}
